2d54ad4fea27b1c7e4ac14f00308ad31a89ffc77
Matthew <unicomatthew@gmail.com>
2023-01-14 16:28:46 -0800

    fixed css to ajust better with smaller screens adding vw to the font size and add more js

--- script.js
+++ script.js
@@ -11,79 +11,128 @@ var questionEl = document.querySelector('#question')
 var containerEL = document.querySelector('.container')
 var answerBoxEl = document.querySelector('#answerBox')
 var Rightanswer;
-var UserAnswer = '';
 
-answerBoxEl.addEventListener('click', function(event){
-  
-    UserAnswer = event.target
-    console.log(UserAnswer);
-    
-    return UserAnswer
-})
+
+var loseCounter = 0;
+
+
+
+
+
+
+
 //var for java
 var secondsLeft = 70;
 
+
+
+
 //began test
-function beganTest(){
+function beganTest() {
     buttonR.disabled = true;
-    secondsLeft 
-   title.innerHTML = 'Good luck'
-   setTime();
+    secondsLeft
+    title.innerHTML = 'Good luck'
+  
+    
+   
+    
+    setTime()
+
+
+        function setTime() {
+
+            var timeInterval = setInterval(function () {
+                secondsLeft--;
+                timeTest.textContent = secondsLeft
+                answerBoxEl.addEventListener('click', function (event) {
+                    hi = event.target.id
+                    console.log(hi);
+                    
+                    if (secondsLeft === 65) {
+                        question2()
+    
+                    }
+                    if (secondsLeft === 60) {
+                        question3(hi)
+                    }
+                    if (secondsLeft === 55) {
+                        question4(hi)
+                    }
+    
+                    if (secondsLeft === 0) {
+                        clearInterval(timeInterval)
+                        title.textContent = "YOU LOST"
+                        buttonR.disabled = false;
+                        secondsLeft = 70;
+                        timeTest.textContent = 5;
+                    }
+                })
+
+
+
+            }, 1000)
+
+        }
+
+
+   
 }
+
 //timer for test
-function setTime(){var timeInterval = setInterval(function(){
-    secondsLeft--;
-    timeTest.textContent = secondsLeft
-    if (secondsLeft === 65){
-        question2()
-    }
-    if (secondsLeft === 60){
-        question3()
-    }
-    if (secondsLeft === 55){
-        question4()
-    }
-
-if (secondsLeft === 0){
-   clearInterval(timeInterval)
-   title.textContent = "YOU LOST"
-   buttonR.disabled = false;
-   secondsLeft = 70;
-   timeTest.textContent = 5;
-}
-
-},1000)
-
-}
 
 
 //functions for questions 
-function question2(){
-    
+function question2(answerBoxEl) {
+
     questionEl.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
     answer1El.textContent = "1. JavaScript";
     answer2El.textContent = "2. terimal/bash";
     answer3El.textContent = "3. for loops ";
     answer4El.textContent = "4. console.log";
-  console.log(answerBoxEl);
 
-    
+    console.log(answerBoxEl);
+
+    if (answerBoxEl == "li4") {
+        console.log('correct2')
+        hi =""
+    }
+    else {
+        console.log("wrong2");
+    }
+
+
 }
 
-function question3(){
+function question3() {
     questionEl.textContent = "String values must be enclosed within ___ when being assigned to vaiables";
     answer1El.textContent = "1. commas";
     answer2El.textContent = "2. curly brackets";
     answer3El.textContent = "3. qoutes";
     answer4El.textContent = "4. parenthesis";
+
+    if (answerBoxEl == "li3") {
+        console.log('correct3')
+    }
+    else {
+        console.log("wrong3");
+    }
+
 }
 
-function question4(){
+function question4() {
     questionEl.textContent = "THe condition in an if / else statement is enclosed with ___.";
     answer1El.textContent = "1. qoutes";
     answer2El.textContent = "2. curly brackets";
     answer3El.textContent = "3. parenthesis";
     answer4El.textContent = "4. square brackets";
+
+    if (answerBoxEl == "li1") {
+        console.log('correct4')
+    }
+    else {
+        console.log("wrong4");
+    }
+
 }
 
 
